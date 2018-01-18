3160e1b87d7c1db3d2e7300f87b44f1f50f489bb
Tom Davies <tomd304@live.co.uk>
2018-01-18 14:16:52 +0000

    Added contact form mailer view

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,8 +1,11 @@
 class ContactMailer < ActionMailer::Base
   default to: tommy3shot@gmail.com
-  
+
   def contact_email(name, email, body)
-    @name = name@email = email@body = body
+    @name = name
+    @email = email
+    @body = body
+    
     mail(from: email, subject: 'Contract Form Message')
   end
 end
